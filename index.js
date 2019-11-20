83130ea42a9bc296bc7b9ccd541e9d21de108093
Joey Killam <jhkillam@gmail.com>
2019-11-19 18:55:22 -0600

    implemented counter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Counter extends React.Component {
+  state = {
+    count: 0
+  }
+  increment() {
+    const currentState = this.state.count
+    const newState = currentState + 1
+    this.setState({ count: newState })
+  }
+  decrement() {
+    const currentState = this.state.count
+    const newState = currentState - 1
+    this.setState({ count: newState })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.increment()}>Increment</button><button onClick={() => this.decrement()}>Decrement</button>
+        <div>
+          {this.state.count}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Counter/>, document.getElementById('root'))
